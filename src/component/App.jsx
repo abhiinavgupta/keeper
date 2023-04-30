a3d44817cf2a31bacedb690d005cf0bc5a569057
abhiinavgupta <121946359+abhiinavgupta@users.noreply.github.com>
2023-04-30 13:23:22 +0530

    first commit

---
+++ src/component/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Note from "./Note";
+import Header from "./Header";
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <Note />
+            <Footer />
+        </>
+    )
+}
